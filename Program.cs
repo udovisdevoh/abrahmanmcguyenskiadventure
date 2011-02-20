1aaf6e83268006823a5654375f4491d98ee8a739
glacasse@gmail.com <glacasse@gmail.com@2444faa4-3864-7405-ca96-f47487f29157>
2011-02-20 23:22:08 +0000

    Added waves from ArtificalArt

--- Program.cs
+++ Program.cs
@@ -1,15 +1,7 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Abrahman McGuyenski
- * Date: 2011-02-21
- * Time: 11:56
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
 using System.Windows.Forms;
 
-namespace AbrahmanAdventure
+namespace AbrahmanAdventure.waves
 {
 	/// <summary>
 	/// Class with program entry point.
@@ -26,6 +18,5 @@ namespace AbrahmanAdventure
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new MainForm());
 		}
-		
 	}
 }
