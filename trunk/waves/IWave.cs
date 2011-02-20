ab4da19e5f7e55c3c57284391ed3b8658df0a8be
glacasse@gmail.com <glacasse@gmail.com@2444faa4-3864-7405-ca96-f47487f29157>
2011-02-20 23:22:08 +0000

    Added waves from ArtificalArt

---
+++ trunk/waves/IWave.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AbrahmanAdventure.waves
+{
+	/// <summary>
+	/// Represent a wave or a wave pack
+	/// </summary>
+	public interface IWave : IEquatable<IWave>
+	{
+		/// <summary>
+        /// Normalize the wave
+        /// </summary>
+        /// <returns>Normalized wave</returns>
+        void Normalize();
+        
+        /// <summary>
+        /// Get amplitude momentum Y value at X
+        /// </summary>
+        /// <param name="x">x coordinates</param>
+        /// <returns>amplitude momentum Y value at X</returns>
+        double this[double x]
+        {
+            get;
+        }
+	}
+}
