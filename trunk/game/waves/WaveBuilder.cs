0e608b0291cc666ce690115136d73a7368be847e
glacasse@gmail.com <glacasse@gmail.com@2444faa4-3864-7405-ca96-f47487f29157>
2011-02-21 18:26:33 +0000

    Now creates much more complex waves

---
+++ trunk/game/waves/WaveBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AbrahmanAdventure.waves
+{
+    internal class WaveBuilder
+    {
+        public WavePack Build(Random random)
+        {
+            WavePack wavePack = new WavePack();
+            //Mountains
+            do
+            {
+                wavePack.Add(this.BuildIndividualWave(32, 512, 16, 48, random, true));
+            } while (random.Next(0, 3) != 0);
+
+            //Platforms
+            do
+            {
+                wavePack.Add(this.BuildIndividualWave(4, 64, 1, 6, random, false));
+            } while (random.Next(0, 3) != 0);
+
+            //Soil
+            do
+            {
+                wavePack.Add(this.BuildIndividualWave(0.05, 2, 0.1, 0.4, random, false));
+            } while (random.Next(0, 5) != 0);
+            
+            return wavePack;
+        }
+
+        private IWave BuildIndividualWave(double minWaveLength, double maxWaveLength, double minAmplitude, double maxAmplitude, Random random, bool isOnlyContinuous)
+        {
+            double waveLength = minWaveLength + random.NextDouble() * (maxWaveLength - minWaveLength);
+            double amplitude = minAmplitude + random.NextDouble() * (maxAmplitude - minAmplitude);
+            double phase = random.NextDouble() * 2.0 - 1.0;
+
+            return new Wave(amplitude, waveLength, phase, WaveFunctions.GetRandomWaveFunction(random, isOnlyContinuous));
+        }
+    }
+}
