6d01068ebb028a384096972d2a02396e5de4f2b2
glacasse@gmail.com <glacasse@gmail.com@2444faa4-3864-7405-ca96-f47487f29157>
2011-02-26 21:33:38 +0000

    More defined surface edge

--- game/level/LevelViewer.cs
+++ game/level/LevelViewer.cs
@@ -94,10 +94,7 @@ namespace AbrahmanAdventure.level
                             int bottomSurfaceAligment = Math.Min(Program.tileSize, ground.TopTexture.Surface.Height);
                             int bottomSurfacePositionY = (relativeFloorHeight + ground.TopTexture.Surface.Height) / bottomSurfaceAligment * bottomSurfaceAligment;
 
-                            for (int bottomTextureBlitCount = 0; bottomTextureBlitCount < 16; bottomTextureBlitCount++)
-                            {
-                                zoneSurface.Blit(ground.BottomTexture.Surface, new Point(x, bottomSurfacePositionY + (ground.BottomTexture.Surface.Height * bottomTextureBlitCount)), new Rectangle(textureInputX, 0, 1, ground.BottomTexture.Surface.Height));
-                            }
+                            zoneSurface.Blit(ground.BottomTexture.Surface, new Point(x, bottomSurfacePositionY), new Rectangle(textureInputX, 0, 1, ground.BottomTexture.Surface.Height));
                         }
                         else
                         {
