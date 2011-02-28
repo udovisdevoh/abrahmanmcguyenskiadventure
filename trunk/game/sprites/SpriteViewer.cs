b76cb54a04430e22915b18d087586ead4fedf3e6
glacasse@gmail.com <glacasse@gmail.com@2444faa4-3864-7405-ca96-f47487f29157>
2011-02-28 22:54:16 +0000

    Showing sprite at the right place: work in progress

--- trunk/game/sprites/SpriteViewer.cs
+++ trunk/game/sprites/SpriteViewer.cs
@@ -52,7 +52,9 @@ namespace AbrahmanAdventure.sprites
                 Surface spriteSurface = sprite.GetCurrentSurface();
 
                 int xBlitPosition = (int)Math.Round(((sprite.XPosition - (spriteSurface.Width / Program.tileSize) / 2.0 - viewOffsetX) * Program.tileSize));
-                int yBlitPosition = Program.totalHeightTileCount * Program.tileSize - ((int)Math.Round((sprite.YPosition - (spriteSurface.Height / Program.tileSize) +viewOffsetY) * Program.tileSize));
+                //int yBlitPosition = Program.totalHeightTileCount * Program.tileSize - ((int)Math.Round((sprite.YPosition - (spriteSurface.Height / Program.tileSize) +viewOffsetY) * Program.tileSize));
+                
+                int yBlitPosition = (int)((sprite.YPosition - viewOffsetY) * (double)Program.tileSize) - spriteSurface.Height;
 
                 mainSurface.Blit(spriteSurface, new Point(xBlitPosition, yBlitPosition));
             }
