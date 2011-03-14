0e127a3731801367e1471784c6ac5e5b735e57ac
glacasse@gmail.com <glacasse@gmail.com@2444faa4-3864-7405-ca96-f47487f29157>
2011-03-14 22:34:22 +0000

    Added math mesh stub

---
+++ game/mathMesh/MathFleshViewer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AbrahmanAdventure.mathMesh
+{
+    /// <summary>
+    /// To view math meshes
+    /// </summary>
+    class MathFleshViewer
+    {
+    }
+}
