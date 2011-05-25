320d053d1d132d96dbe8ca4b1491a50a9857f6fb
glacasse@gmail.com <glacasse@gmail.com@2444faa4-3864-7405-ca96-f47487f29157>
2011-05-25 21:55:19 +0000

    Fixed a bug with sound

--- trunk/game/audio/SoundManager.cs
+++ trunk/game/audio/SoundManager.cs
@@ -11,32 +11,34 @@ namespace AbrahmanAdventure.audio
     internal static class SoundManager
     {
         #region Fields and parts
-        private static SoundPlayer jumpSound = new SoundPlayer("./assets/sounds/Jump.wav");
+        private static SoundPlayer jumpSound;
 
-        private static SoundPlayer jumpDownSound = new SoundPlayer("./assets/sounds/JumpDown.wav");
+        private static SoundPlayer jumpDownSound;
 
-        private static SoundPlayer hitSound = new SoundPlayer("./assets/sounds/Hit.wav");
+        private static SoundPlayer hitSound;
 
-        private static SoundPlayer hit2Sound = new SoundPlayer("./assets/sounds/Hit2.wav");
+        private static SoundPlayer hit2Sound;
 
-        private static SoundPlayer koSound = new SoundPlayer("./assets/sounds/Ko.wav");
+        private static SoundPlayer koSound;
 
-        private static SoundPlayer ko2Sound = new SoundPlayer("./assets/sounds/Ko2.wav");
+        private static SoundPlayer ko2Sound;
 
-        private static SoundPlayer attemptSound = new SoundPlayer("./assets/sounds/Attempt.wav");
+        private static SoundPlayer attemptSound;
 
-        private static SoundPlayer punchSound = new SoundPlayer("./assets/sounds/Punch.wav");
+        private static SoundPlayer punchSound;
         #endregion
 
         #region Constructors
         static SoundManager()
         {
             jumpSound = LoadSound("./assets/sounds/Jump.wav");
+            jumpDownSound = LoadSound("./assets/sounds/JumpDown.wav");
             hitSound = LoadSound("./assets/sounds/Hit.wav");
             hit2Sound = LoadSound("./assets/sounds/Hit2.wav");
             koSound = LoadSound("./assets/sounds/Ko.wav");
             ko2Sound = LoadSound("./assets/sounds/Ko2.wav");
             attemptSound = LoadSound("./assets/sounds/Attempt.wav");
+            punchSound = LoadSound("./assets/sounds/Punch.wav");
         }
 
         private static SoundPlayer LoadSound(string fileName)
