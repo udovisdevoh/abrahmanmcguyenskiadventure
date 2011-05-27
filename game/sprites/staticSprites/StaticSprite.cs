af3dc73cc8e9d411a76550f2a8ec7ed6ca0294a3
glacasse@gmail.com <glacasse@gmail.com@2444faa4-3864-7405-ca96-f47487f29157>
2011-05-27 14:21:29 +0000

    Moved some sprite classes Added abstract StaticSprite class

---
+++ game/sprites/staticSprites/StaticSprite.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AbrahmanAdventure.sprites
+{
+    /// <summary>
+    /// Sprite not moving, not affected by gravity
+    /// </summary>
+    abstract class StaticSprite : AbstractSprite
+    {
+        public StaticSprite(double xPosition, double yPosition, Random random)
+            : base(xPosition, yPosition, random)
+        {
+        }
+    }
+}
