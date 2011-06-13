a98877db94f1804f94ec1f34522eeeee6df8d608
glacasse@gmail.com <glacasse@gmail.com@2444faa4-3864-7405-ca96-f47487f29157>
2011-06-13 19:42:51 +0000

    Slowed down crystal balls

--- trunk/game/sprites/projectiles/CrystalBallSprite.cs
+++ trunk/game/sprites/projectiles/CrystalBallSprite.cs
@@ -160,12 +160,12 @@ namespace AbrahmanAdventure.sprites
 
         protected override double BuildMaxWalkingSpeed()
         {
-            return 0.40;
+            return 0.30;
         }
 
         protected override double BuildMaxRunningSpeed()
         {
-            return 0.40;
+            return 0.30;
         }
 
         protected override double BuildStartingJumpAcceleration()
