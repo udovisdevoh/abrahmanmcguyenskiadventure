92cb111a36302640e0093034a9e37e5d80583e3d
glacasse@gmail.com <glacasse@gmail.com@2444faa4-3864-7405-ca96-f47487f29157>
2011-06-16 17:30:07 +0000

    We can now spawn beavers using Anarchy Blocks

---
+++ game/sprites/powerups/BeaverSprite.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SdlDotNet.Graphics;
+
+namespace AbrahmanAdventure.sprites
+{
+    /// <summary>
+    /// A beaver (similar behavior to Yoshi)
+    /// </summary>
+    internal class BeaverSprite : MonsterSprite, IGrowable
+    {
+        #region Fields and parts
+        private static Surface standRight;
+
+        private static Surface standLeft;
+
+        private static Surface walkRight;
+
+        private static Surface walkLeft;
+
+        private static Surface hitRight;
+
+        private static Surface hitLeft;
+
+        private static Surface dead;
+
+        /// <summary>
+        /// Cycle of growth
+        /// </summary>
+        private Cycle growthCycle;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Create sprite
+        /// </summary>
+        /// <param name="xPosition">x position</param>
+        /// <param name="yPosition">y position</param>
+        /// <param name="random">random number generator</param>
+        public BeaverSprite(double xPosition, double yPosition, Random random)
+            : base(xPosition, yPosition, random)
+        {
+            growthCycle = new Cycle(Program.powerUpGrowthTime, false);
+            if (standLeft == null)
+            {
+                standRight = BuildSpriteSurface("./assets/rendered/beaver/BeaverStand.png");
+                standLeft = standRight.CreateFlippedHorizontalSurface();
+
+                walkRight = BuildSpriteSurface("./assets/rendered/beaver/BeaverWalk.png");
+                walkLeft = walkRight.CreateFlippedHorizontalSurface();
+
+                hitRight = BuildSpriteSurface("./assets/rendered/beaver/BeaverHit.png");
+                hitLeft = hitRight.CreateFlippedHorizontalSurface();
+
+                dead = hitRight.CreateFlippedVerticalSurface();
+            }
+        }
+        #endregion
+
+        #region Override
+        protected override double BuildMaxHealth()
+        {
+            return 100;
+        }
+
+        protected override double BuildJumpingTime()
+        {
+            return 0;
+        }
+
+        protected override double BuildWalkingCycleLength()
+        {
+            return 5;
+        }
+
+        protected override double BuildWalkingAcceleration()
+        {
+            return 0.01;
+        }
+
+        protected override double BuildMaxWalkingSpeed()
+        {
+            return 0.55;
+        }
+
+        protected override double BuildMaxRunningSpeed()
+        {
+            return 0.85;
+        }
+
+        protected override double BuildStartingJumpAcceleration()
+        {
+            return 25.0;
+        }
+
+        protected override double BuildAttackingTime()
+        {
+            return 4;
+        }
+
+        protected override double BuildHitTime()
+        {
+            return 0;
+        }
+
+        protected override double BuildAttackStrengthCollision()
+        {
+            return 0;
+        }
+
+        protected override double BuildWidth(Random random)
+        {
+            return 1.5;
+        }
+
+        protected override double BuildHeight(Random random)
+        {
+            return 0.8;
+        }
+
+        protected override double BuildBounciness()
+        {
+            return 1.0;
+        }
+
+        protected override double BuildSafeDistanceAi()
+        {
+            return 0.0;
+        }
+
+        protected override bool BuildIsCanDoDamageToPlayerWhenTouched()
+        {
+            return false;
+        }
+
+        protected override bool BuildIsAnnihilateOnExitScreen()
+        {
+            return false;
+        }
+
+        protected override bool BuildIsAiEnabled()
+        {
+            return false;
+        }
+
+        protected override bool BuildIsCanJump(Random random)
+        {
+            return false;
+        }
+
+        protected override bool BuildIsAvoidFall(Random random)
+        {
+            return false;
+        }
+
+        protected override bool BuildIsToggleWalkWhenJumpedOn()
+        {
+            return false;
+        }
+
+        protected override bool BuildIsFleeWhenAttacked(Random random)
+        {
+            return false;
+        }
+
+        protected override bool BuildIsFullSpeedAfterBounceNoAi()
+        {
+            return true;
+        }
+
+        protected override bool BuildIsInstantKickConvertedSprite()
+        {
+            return false;
+        }
+
+        protected override bool BuildIsEnableSpontaneousConversion()
+        {
+            return false;
+        }
+
+        protected override bool BuildIsEnableJumpOnConversion()
+        {
+            return false;
+        }
+
+        protected override bool BuildIsNoAiChangeDirectionWhenStucked()
+        {
+            return true;
+        }
+
+        protected override bool BuildIsNoAiDieWhenStucked()
+        {
+            return false;
+        }
+
+        protected override bool BuildIsNoAiAlwaysBounce()
+        {
+            return false;
+        }
+
+        protected override bool BuildIsJumpableOn()
+        {
+            return true;
+        }
+
+        protected override bool BuildIsNoAiChangeDirectionByCycle()
+        {
+            return false;
+        }
+
+        protected override bool BuildIsDieOnTouchGround()
+        {
+            return false;
+        }
+
+        protected override bool BuildIsMakeSoundWhenTouchGround()
+        {
+            return false;
+        }
+
+        protected override double BuildChangeDirectionNoAiCycleLength()
+        {
+            return 100;
+        }
+
+        protected override double BuildJumpProbability()
+        {
+            return 0;
+        }
+
+        public override AbstractSprite GetConverstionSprite(Random random)
+        {
+            return null;
+        }
+
+        public override Surface GetCurrentSurface(out double xOffset, out double yOffset)
+        {
+            xOffset = yOffset = 0;
+
+            int cycleDivision = WalkingCycle.GetCycleDivision(4.0);
+
+            if (!IsAlive)
+                return dead;
+
+            if (cycleDivision == 1 || cycleDivision == 3)
+            {
+                if (IsTryingToWalkRight)
+                    return walkRight;
+                else
+                    return walkLeft;
+            }
+            else
+            {
+                if (HitCycle.IsFired)
+                {
+                    if (IsTryingToWalkRight)
+                        return hitRight;
+                    else
+                        return hitLeft;
+                }
+                else
+                {
+                    if (IsTryingToWalkRight)
+                        return standRight;
+                    else
+                        return standLeft;
+                }
+            }
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Cycle of growth
+        /// </summary>
+        public Cycle GrowthCycle
+        {
+            get { return growthCycle; }
+        }
+        #endregion
+    }
+}
