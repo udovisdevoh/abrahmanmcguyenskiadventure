a86b204b7ed0da4184aac384eff52788d5e8cfe6
glacasse@gmail.com <glacasse@gmail.com@2444faa4-3864-7405-ca96-f47487f29157>
2011-06-24 02:48:54 +0000

    Added corns (but they must be thrown by farmers)

--- game/GameState.cs
+++ game/GameState.cs
@@ -154,6 +154,7 @@ namespace AbrahmanAdventure
             spritePopulation.Add(new MormonSprite(-55, Program.totalHeightTileCount / -2, random));
             spritePopulation.Add(new GypsySprite(-75, Program.totalHeightTileCount / -2, random));
             spritePopulation.Add(new DoctorSprite(-85, Program.totalHeightTileCount / -2, random));
+            spritePopulation.Add(new CornSprite(-100, Program.totalHeightTileCount / -2, random));
 
             spritePopulation.Add(new BrickSprite(-10, -10, random, true));
             spritePopulation.Add(new BrickSprite(-11, -10, random, true));
