755b742e83f3eeb7faabb4a664391d556a68c397
glacasse@gmail.com <glacasse@gmail.com@2444faa4-3864-7405-ca96-f47487f29157>
2011-06-29 21:55:04 +0000

    Drill sprite: work in progress

---
+++ trunk/game/physics/UpDownCycleMoveManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AbrahmanAdventure.sprites;
+
+namespace AbrahmanAdventure.physics
+{
+    /// <summary>
+    /// Manages sprites like drills (piranha plant behavior)
+    /// </summary>
+    internal class UpDownCycleMoveManager
+    {
+        internal void update(IUpDownCycleMove upDownMovingSprite, AbstractSprite playerSprite, double timeDelta)
+        {
+            if (upDownMovingSprite.UpDownCycle.CurrentValue < upDownMovingSprite.AlwaysActiveRangeCycleStart)
+                upDownMovingSprite.UpDownCycle.Increment(timeDelta);
+            else if (upDownMovingSprite.UpDownCycle.CurrentValue > upDownMovingSprite.AlwaysActiveRangeCycleStop)
+                upDownMovingSprite.UpDownCycle.Increment(timeDelta);
+            else if (upDownMovingSprite.IGround != playerSprite.IGround)
+            {
+                if (!upDownMovingSprite.IsUseDontMoveUpDistance)
+                    upDownMovingSprite.UpDownCycle.Increment(timeDelta);
+                else if (Math.Abs(upDownMovingSprite.XPosition - playerSprite.XPosition) > upDownMovingSprite.DontMoveUpDistance)
+                    upDownMovingSprite.UpDownCycle.Increment(timeDelta);
+            }
+        }
+    }
+}
