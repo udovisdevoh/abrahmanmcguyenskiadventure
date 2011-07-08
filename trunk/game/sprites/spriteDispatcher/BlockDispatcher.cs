153dad494f63c142a86e2ef6a66fb9eeda67e1e5
glacasse@gmail.com <glacasse@gmail.com@2444faa4-3864-7405-ca96-f47487f29157>
2011-07-08 17:06:11 +0000

    Block dispatcher: work in progress

--- trunk/game/sprites/spriteDispatcher/BlockDispatcher.cs
+++ trunk/game/sprites/spriteDispatcher/BlockDispatcher.cs
@@ -20,9 +20,38 @@ namespace AbrahmanAdventure.sprites
         /// <param name="random">random number generator</param>
         internal static void DispatchBlocks(Level level, SpritePopulation spritePopulation, Random random)
         {
-            AbstractWave yDistanceFromGroundWave = WaveBuilder.BuildBlockYDistanceFromGroundWave(random);
-            AbstractWave segmentWidthWave = WaveBuilder.BuildBlockSegmentWidthWave(random);
-            AbstractWave xSegmentDistanceWave = WaveBuilder.BuildXBlockSegmentDistanceWave(random);
+            HashSet<int> addedBlockMemory = new HashSet<int>();
+
+            //AbstractWave segmentWidthWave = WaveBuilder.BuildBlockSegmentWidthWave(random);
+            //AbstractWave xSegmentDistanceWave = WaveBuilder.BuildXBlockSegmentDistanceWave(random);
+
+            double yPosition;
+            //double segmentBeingDrawnCurrentWidth = 0.0;
+            //double desiredSegmentWidth;
+            foreach (Ground ground in level)
+            {
+                AbstractWave yDistanceFromGroundWave = WaveBuilder.BuildBlockYDistanceFromGroundWave(random);
+
+                for (double xPosition = level.LeftBound; xPosition < level.RightBound; xPosition++)
+                {
+                    double yOffset = yDistanceFromGroundWave[xPosition];
+
+                    if (yOffset > 0)
+                        continue;
+
+                    //desiredSegmentWidth = segmentWidthWave[xPosition];
+                    yPosition = Math.Round(ground[xPosition] + yOffset + 2.0);
+                    //segmentBeingDrawnCurrentWidth++;
+
+                    int uniqueBlockKey = (int)xPosition + (int)yPosition * 100000;
+
+                    if (!addedBlockMemory.Contains(uniqueBlockKey))
+                    {
+                        spritePopulation.Add(new BrickSprite(xPosition, yPosition, random, true));
+                        addedBlockMemory.Add(uniqueBlockKey);
+                    }
+                }
+            }
         }
         #endregion
     }
