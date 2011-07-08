c2f08ffdae84e09d87649a528e4f332492f20805
glacasse@gmail.com <glacasse@gmail.com@2444faa4-3864-7405-ca96-f47487f29157>
2011-07-08 16:37:41 +0000

    Block dispatcher: work in progress

---
+++ game/sprites/spriteDispatcher/BlockDispatcher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AbrahmanAdventure.level;
+
+namespace AbrahmanAdventure.sprites
+{
+    /// <summary>
+    /// Dispatch blocks
+    /// </summary>
+    internal static class BlockDispatcher
+    {
+        #region Internal Methods
+        /// <summary>
+        /// Dispatch blocks
+        /// </summary>
+        /// <param name="level">level</param>
+        /// <param name="spritePopulation">sprite population</param>
+        /// <param name="random">random number generator</param>
+        internal static void DispatchBlocks(Level level, SpritePopulation spritePopulation, Random random)
+        {
+            AbstractWave yDistanceFromGroundWave = WaveBuilder.BuildBlockYDistanceFromGroundWave(random);
+            AbstractWave segmentWidthWave = WaveBuilder.BuildBlockSegmentWidthWave(random);
+            AbstractWave xSegmentDistanceWave = WaveBuilder.BuildXBlockSegmentDistanceWave(random);
+        }
+        #endregion
+    }
+}
