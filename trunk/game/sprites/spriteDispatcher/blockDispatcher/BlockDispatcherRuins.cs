7845c6a6326c1744e07ac4aae0b05d02b016253e
glacasse@gmail.com <glacasse@gmail.com@2444faa4-3864-7405-ca96-f47487f29157>
2011-07-14 18:53:23 +0000

    Added ruin block dispatcher stub

---
+++ trunk/game/sprites/spriteDispatcher/blockDispatcher/BlockDispatcherRuins.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AbrahmanAdventure.level;
+using AbrahmanAdventure.physics;
+
+namespace AbrahmanAdventure.sprites
+{
+    /// <summary>
+    /// Dispatches blocks
+    /// </summary>
+    internal static class BlockDispatcherRuins
+    {
+        #region Internal Methods
+        /// <summary>
+        /// Dispatch blocks
+        /// </summary>
+        /// <param name="ground">ground</param>
+        /// <param name="level">level</param>
+        /// <param name="spritePopulation">sprite population</param>
+        /// <param name="addedBlockMemory">to remember blocks that are already there</param>
+        /// <param name="random">random number generator</param>
+        internal static void DispatchBlocks(Ground ground, Level level, SpritePopulation spritePopulation, HashSet<int> addedBlockMemory, Random random)
+        {
+        }
+        #endregion
+    }
+}
