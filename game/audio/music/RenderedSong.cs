1dbb4b47c61f4ba9ef34f682521d8f5940513e85
glacasse@gmail.com <glacasse@gmail.com@2444faa4-3864-7405-ca96-f47487f29157>
2011-07-17 21:10:46 +0000

    Music player: work in progress

---
+++ game/audio/music/RenderedSong.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AbrahmanAdventure.audio.Midi;
+
+namespace AbrahmanAdventure.audio
+{
+    /// <summary>
+    /// Rendered song
+    /// </summary>
+    internal class RenderedSong
+    {
+        #region Field and parts
+        /// <summary>
+        /// Current position in list of messages
+        /// </summary>
+        private int listMessagePointer;
+
+        /// <summary>
+        /// List of midi messages
+        /// </summary>
+        private List<ChannelMessage> listMessages;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Create rendered song from song
+        /// </summary>
+        /// <param name="song">song</param>
+        public RenderedSong(Song song)
+        {
+            listMessages = new List<ChannelMessage>();
+        }
+        #endregion
+    }
+}
