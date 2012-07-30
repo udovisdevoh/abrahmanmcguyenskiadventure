8de4433865546fcaaef5fceff1427d2c5f070839
glacasse <glacasse@2444faa4-3864-7405-ca96-f47487f29157>
2012-07-30 21:50:22 +0000

    Added clockwork stub

---
+++ trunk/game/sprites/clockwork/AbstractLinkage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SdlDotNet.Graphics;
+
+namespace AbrahmanAdventure.sprites
+{
+    /// <summary>
+    /// Represents a mechanical component. It could be linkage on a wheel, a pendulum, a seesaw, an aerial lift's attachment or a liana
+    /// Platforms or other linkages are attached to it. (platforms and flail balls are linkages)
+    /// Linkages can be attached to "rope-grounds", elevator's vertical line, or can be held in the void, or can be affected by gravity and use the platforms as legs
+    /// </summary>
+    internal abstract class AbstractLinkage : AbstractSprite
+    {
+
+    }
+}
