7993958f2f98cb8c14d96b3e78fa7f607974b65c
glacasse <glacasse@2444faa4-3864-7405-ca96-f47487f29157>
2012-08-01 02:33:13 +0000

    clockwork: work in progress

--- trunk/game/sprites/clockwork/AbstractLinkage.cs
+++ trunk/game/sprites/clockwork/AbstractLinkage.cs
@@ -13,6 +13,76 @@ namespace AbrahmanAdventure.sprites
     /// </summary>
     internal abstract class AbstractLinkage : AbstractSprite
     {
+        #region Protected Override
+        protected override bool BuildIsCrossGrounds()
+        {
+            return true;
+        }
 
+        protected override bool BuildIsImpassable()
+        {
+            return true;
+        }
+
+        protected override bool BuildIsAnnihilateOnExitScreen()
+        {
+            return false;
+        }
+
+        protected override bool BuildIsVulnerableToPunch()
+        {
+            return false;
+        }
+
+        protected override double BuildMaxHealth()
+        {
+            return 100;
+        }
+
+        protected override double BuildJumpingTime()
+        {
+            return 0;
+        }
+
+        protected override double BuildWalkingCycleLength()
+        {
+            return 0;
+        }
+
+        protected override double BuildWalkingAcceleration()
+        {
+            return 0;
+        }
+
+        protected override double BuildMaxWalkingSpeed()
+        {
+            return 0;
+        }
+
+        protected override double BuildMaxRunningSpeed()
+        {
+            return 0;
+        }
+
+        protected override double BuildStartingJumpAcceleration()
+        {
+            return 0;
+        }
+
+        protected override double BuildAttackingTime()
+        {
+            return 0;
+        }
+
+        protected override double BuildHitTime()
+        {
+            return 0;
+        }
+
+        protected override double BuildAttackStrengthCollision()
+        {
+            return 0;
+        }
+        #endregion
     }
 }
---
+++ trunk/game/sprites/clockwork/Wheel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SdlDotNet.Graphics;
+
+namespace AbrahmanAdventure.sprites
+{
+    /// <summary>
+    /// Represents a wheel and its bearing
+    /// </summary>
+    #warning Eventually remove abstract keyword
+    internal abstract class Wheel : AbstractLinkage
+    {
+        protected override bool BuildIsAffectedByGravity()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override double BuildWidth(Random random)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override double BuildHeight(Random random)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override double BuildBounciness()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override double BuildMaxFallingSpeed()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override int BuildZIndex()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string BuildTutorialComment()
+        {
+            return "Play with that ferris wheel, it's fun. Be careful when there are flail balls attached to it.";
+        }
+
+        public override Surface GetCurrentSurface(out double xOffset, out double yOffset)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
