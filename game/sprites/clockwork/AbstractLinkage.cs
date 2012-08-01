1875368e2a8a9d92d2fadf0cef1e99c1faf329d1
glacasse <glacasse@2444faa4-3864-7405-ca96-f47487f29157>
2012-08-01 11:58:12 +0000

    Clockwork: work in progress

--- game/sprites/clockwork/AbstractLinkage.cs
+++ game/sprites/clockwork/AbstractLinkage.cs
@@ -7,7 +7,7 @@ using SdlDotNet.Graphics;
 namespace AbrahmanAdventure.sprites
 {
     /// <summary>
-    /// Represents a mechanical component. It could be linkage on a wheel, a pendulum, a seesaw, an aerial lift's attachment or a liana
+    /// Represents a mechanical component. It could be linkage on a wheel, a pendulum, a seesaw, an aerial lift's support or a liana
     /// Platforms or other linkages are attached to it. (platforms and flail balls are linkages)
     /// Linkages can be attached to "rope-grounds", elevator's vertical line, or can be held in the void, or can be affected by gravity and use the platforms as legs
     /// </summary>
---
+++ game/sprites/clockwork/FlailBall.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SdlDotNet.Graphics;
+
+namespace AbrahmanAdventure.sprites
+{
+    /// <summary>
+    /// Represents a flail ball attached to a linkage
+    /// </summary>
+    #warning Eventually remove abstract keyword
+    internal abstract class FlailBall : AbstractLinkage
+    {
+        protected override bool BuildIsAffectedByGravity()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override double BuildWidth(Random random)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override double BuildHeight(Random random)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override double BuildBounciness()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override double BuildMaxFallingSpeed()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override int BuildZIndex()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string BuildTutorialComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Surface GetCurrentSurface(out double xOffset, out double yOffset)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
