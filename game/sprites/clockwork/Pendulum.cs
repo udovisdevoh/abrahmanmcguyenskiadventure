442cc754ba4c0aa303af5e6398503dd756068a73
glacasse <glacasse@2444faa4-3864-7405-ca96-f47487f29157>
2012-08-03 15:20:02 +0000

    clockwork: work in progress

--- game/sprites/clockwork/Pendulum.cs
+++ game/sprites/clockwork/Pendulum.cs
@@ -12,27 +12,23 @@ namespace AbrahmanAdventure.sprites
     #warning Eventually remove abstract keyword
     internal abstract class Pendulum : AbstractLinkage
     {
+        #region Override
         protected override bool BuildIsAffectedByGravity()
         {
-            throw new NotImplementedException();
+            return false;
         }
 
         protected override double BuildWidth(Random random)
         {
-            throw new NotImplementedException();
+            return 1.0;
         }
 
         protected override double BuildHeight(Random random)
         {
-            throw new NotImplementedException();
+            return 1.0;
         }
 
         protected override double BuildBounciness()
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override double BuildMaxFallingSpeed()
         {
             throw new NotImplementedException();
         }
@@ -42,14 +38,50 @@ namespace AbrahmanAdventure.sprites
             return "Play with that pendulum, it's fun! Be careful when there are flail balls attached to it.";
         }
 
+        public override double BuildSupportHeight()
+        {
+            return 0;
+        }
+
         public override Surface GetCurrentSurface(out double xOffset, out double yOffset)
         {
             throw new NotImplementedException();
         }
+        #endregion
 
-        public override double BuildSupportHeight()
+        #region Constructor
+        /// <summary>
+        /// Do not use that constructor
+        /// </summary>
+        public Pendulum()
         {
-            throw new NotImplementedException();
         }
+
+        /// <summary>
+        /// Build an abstract linkage (wheel, pendulum, seesaw, lift, platform, liana)
+        /// </summary>
+        /// <param name="xPosition">x position</param>
+        /// <param name="yPosition">y position</param>
+        /// <param name="random">random number generator</param>
+        public Pendulum(double xPosition, double yPosition, Random random)
+            : base(xPosition, yPosition, random)
+        {   
+        }
+
+        /// <summary>
+        /// Build an abstract linkage (wheel, pendulum, seesaw, lift, platform, liana)
+        /// </summary>
+        /// <param name="xPosition">x position</param>
+        /// <param name="yPosition">y position</param>
+        /// <param name="random">random number generator</param>
+        /// <param name="isAffectedByGravity">whether wheel is affected by gravity (default: false)</param>
+        /// <param name="supportHeight">support's height (default: 0)</param>
+        public Pendulum(double xPosition, double yPosition, Random random, bool isAffectedByGravity, double supportHeight)
+            : this(xPosition, yPosition, random)
+        {
+            this.IsAffectedByGravity = isAffectedByGravity;
+            this.SupportHeight = supportHeight;
+        }
+        #endregion
     }
 }
